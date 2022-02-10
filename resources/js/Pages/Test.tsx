d33bbda5bc687ef8e6cd9c220abb99883328abc5
Paul Estala <pestala495@tutanota.com>
2022-02-10 06:58:11 -0700

    inertia/react/ts setup and laravel cleanup

---
+++ resources/js/Pages/Test.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Head } from '@inertiajs/inertia-react'
+
+export default function Test() {
+    return (
+        <>
+            <Head title="Welcome" />
+            <h1>Bienvenido</h1>
+            <p>Hello, welcome to your first Inertia app!</p>
+        </>
+    )
+}
