2d08fc3f0f5827d8b5dffa921de0ad764eb5b608
Paul Estala <pestala495@tutanota.com>
2022-02-12 13:47:31 -0700

    more wip

--- resources/js/Pages/About.tsx
+++ resources/js/Pages/About.tsx
@@ -1,4 +1,4 @@
-import {Head} from '@inertiajs/inertia-react'
+import { Head } from '@inertiajs/inertia-react'
 import { AboutProps } from '../types/props'
 import Unauthenticated from './Layouts/Unauthenticated'
 
@@ -7,12 +7,22 @@ export default function About(props: AboutProps) {
         <Unauthenticated>
             <Head>
                 <title>About</title>
-                <meta
-                    name="description"
-                    content=""
-                />
+                <meta name="description" content="Learn some basic information about Paul Estala." />
             </Head>
-            <p>About</p>
+            <div className="flex w-full px-24 pt-16 justify-evenly">
+                <div className="flex items-center justify-center w-1/2 px-24 opacity-0 fade-in-delayed">
+                    <div>
+                        <p className="text-xl">
+                            I&apos;m Paul Estala a Software Engineer from Chihuahua, Mexico.
+                            I code for a living as well as for fun. If I&apos;m not coding,
+                            I like to design things or do some sports.
+                        </p>
+                    </div>
+                </div>
+                <div className="flex justify-center w-1/2">
+                    <img className="w-96 animate-fade-in-from-bigger" src="/img/logo.svg" alt="" />
+                </div>
+            </div>
         </Unauthenticated>
     )
 }
