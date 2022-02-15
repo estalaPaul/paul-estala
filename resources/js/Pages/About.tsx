1be0422ba1e62097426d3a57f6a96fd670890e70
Paul Estala <pestala495@tutanota.com>
2022-02-14 19:03:43 -0700

    more wip

--- resources/js/Pages/About.tsx
+++ resources/js/Pages/About.tsx
@@ -6,28 +6,41 @@ export default function About(props: AboutProps) {
     const { age } = props
 
     return (
-        <Unauthenticated>
+        <>
             <Head>
                 <title>About</title>
                 <meta name="description" content="Learn some basic information about Paul Estala." />
             </Head>
-            <div className="flex w-full px-24 pt-16 justify-evenly">
-                <div className="flex items-center justify-center w-1/2 px-24 opacity-0 fade-in-delayed">
-                    <div>
-                        <p className="text-2xl">
-                            I&apos;m Paul Estala, a {age} year old Software Engineer from Chihuahua, Mexico.
-                        </p>
-                        <p className="text-gray text-2xl mt-5">
-                            Currently coding full time for a living but also as a hobby.
-                            If I&apos;m not coding, I&apos;m probably spending time with
-                            my family or doing some sort of exercise.
-                        </p>
+            <Unauthenticated>
+                <div className="h-screen flex items-center">
+                    <div className="flex w-full px-24 justify-evenly">
+                        <div className="flex items-center justify-center w-1/2 px-24 opacity-0 fade-in-delayed">
+                            <div>
+                                <p className="text-2xl">
+                                    I&apos;m Paul Estala, a {age} year old Software Engineer from Chihuahua, Mexico.
+                                </p>
+                                <p className="text-gray text-2xl mt-5 mb-10">
+                                    Currently coding full time for a living but also as a hobby.
+                                    If I&apos;m not coding, I&apos;m probably spending time with
+                                    my family or doing some sort of exercise.
+                                </p>
+                                <a
+                                    className="items-center gap-2 text-2xl text-gray border-b-2 border-white transition-color duration-300 hover:text-black hover:border-black hover:brightness-0 hover:saturate-0"
+                                    href="#skills"
+                                >
+                                    See some of the tech I work with
+                                    <img className="inline ml-2 mb-0.5" src="/img/icons/double-down.svg" alt="" />
+                                </a>
+                            </div>
+                        </div>
+                        <div className="flex justify-center w-1/2 items-center">
+                            <div className="overflow-hidden w-80 h-80 rounded-full">
+                                <img className="h-full w-full" src="/img/headshot.webp" alt="Photo of Paul Estala" />
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="flex justify-center w-1/2">
-                    {/*TODO: Pick photo of you and add it here*/}
-                </div>
-            </div>
-        </Unauthenticated>
+            </Unauthenticated>
+        </>
     )
 }
--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -6,29 +6,31 @@ export default function Home(props: HomeProps) {
     const { years } = props
 
     return (
-        <div className="h-screen">
-            <Unauthenticated>
-                <Head>
-                    <title>Home</title>
-                    <meta
-                        name="description"
-                        content="Paul Estala&apos;s portfolio website showcasing his programming and design projects, as well as his career as a Software Engineer and some of his personal hobbies."
-                    />
-                </Head>
-                <div className="flex w-full px-24 pt-16 justify-evenly">
-                    <div className="flex items-center justify-center w-1/2 px-24 opacity-0 fade-in-delayed">
-                        <div>
-                            <p className="mb-5 text-6xl font-bold">Hey there!</p>
-                            <p className="text-xl">
-                                I&apos;m a <span className="font-semibold">software engineer</span> specializing in <span className="font-semibold">full stack web development</span> with {years} years of professional experience.
-                            </p>
+        <>
+            <Head>
+                <title>Home</title>
+                <meta
+                    name="description"
+                    content="Paul Estala&apos;s portfolio website showcasing his programming and design projects, as well as his career as a Software Engineer and some of his personal hobbies."
+                />
+            </Head>
+            <div className="h-screen">
+                <Unauthenticated>
+                    <div className="flex w-full px-24 pt-16 justify-evenly">
+                        <div className="flex items-center justify-center w-1/2 px-24 opacity-0 fade-in-delayed">
+                            <div>
+                                <p className="mb-5 text-6xl font-bold">Hey there!</p>
+                                <p className="text-xl">
+                                    I&apos;m a <span className="font-semibold">software engineer</span> specializing in <span className="font-semibold">full stack web development</span> with {years} years of professional experience.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex justify-center w-1/2">
+                            <img className="w-96 animate-fade-in-from-bigger" src="/img/logo.svg" alt="" />
                         </div>
                     </div>
-                    <div className="flex justify-center w-1/2">
-                        <img className="w-96 animate-fade-in-from-bigger" src="/img/logo.svg" alt="" />
-                    </div>
-                </div>
-            </Unauthenticated>
-        </div>
+                </Unauthenticated>
+            </div>
+        </>
     )
 }
