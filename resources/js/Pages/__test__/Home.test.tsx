745d734c0ff542315b8084a82bdd1b6e4b30283d
Paul Estala <pestala495@tutanota.com>
2022-03-11 22:59:07 -0700

    remove jest

--- resources/js/Pages/__test__/Home.test.tsx
+++
@@ -1,8 +0,0 @@
-import Home from '../Home'
-import { render } from '@testing-library/react'
-
-test('Homepage renders correctly', () => {
-    const { getByText } = render(<Home years='22' />)
-
-    getByText('Hey there!')
-})
